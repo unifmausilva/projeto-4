9d2e6e5d3a9660f0c6e96aa94aaefadb6c7ae9e3
unifmausilva <161088037+unifmausilva@users.noreply.github.com>
2024-04-29 23:41:04 +0000

    implementando função cadastrar

--- clientes.c
+++ clientes.c
@@ -2,5 +2,13 @@
 #include "clientes.h"
 
 void cadastrar_cliente(){
+  printf("===========================");
+  printf("CADASTRAR NOVO CLIENTE\n");
+  printf("Digite sua razão social:\n");
+  printf("Digite seu CNPJ:\n");
+  if (printf("Digite qual o tipo de conta: ")){
+    printf("1 - Conta corrente\n");
+    
+  }
   
 }
