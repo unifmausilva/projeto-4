f394deee2d4ee540df99962cd0b632f4c2e5a335
unifmausilva <161088037+unifmausilva@users.noreply.github.com>
2024-05-03 22:38:06 +0000

    escolha de conta

--- clientes.c
+++ clientes.c
@@ -1,14 +1,34 @@
 #include <stdio.h>
 #include "clientes.h"
 
+
+void escolhaconta(){
+ int escolha;
+  printf("-----TIPOS DE CONTA-----\n");
+  printf("1 - Conta Comum ");
+  printf("\n");
+  printf("2 - Conta Plus ");
+  if(scanf("%d", &escolha) != 1){
+    printf("Erro ao ler a opção\n");
+    
+  }
+  if (escolha == 1){
+    printf("Você escolheu a conta comum\n");
+  }else if(escolha == 2){
+    printf("Você escolheu a conta plus\n");
+  }else{
+    printf("Opção inválida, tente novamente\n");
+  }
+  
+  
+}
+
 void cadastrar_cliente(){
   printf("===========================");
   printf("CADASTRAR NOVO CLIENTE\n");
   printf("Digite sua razão social:\n");
   printf("Digite seu CNPJ:\n");
-  if (printf("Digite qual o tipo de conta: ")){
-    printf("1 - Conta corrente\n");
-    
-  }
+  printf("Digite qual conta você deseja abrir:\n");
+  escolhaconta();
   
 }
