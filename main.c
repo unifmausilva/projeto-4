b84bc49387ab38843003d23c650e5101e7bd7b36
unifmausilva <161088037+unifmausilva@users.noreply.github.com>
2024-05-10 23:34:07 +0000

    atualizando

--- main.c
+++ main.c
@@ -1,26 +1,27 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "banco.h"
-
-int main() {
-    Cliente clientes[MAX_CLIENTES];
-    int total_clientes = carregarClientes(clientes);
-
-    int opcao;
-    do {
-        printf("\nMenu de Opcoes:\n");
-        printf("1. Novo cliente\n");
-        printf("2. Apagar cliente\n");
-        printf("3. Listar clientes\n");
-        // Adicione aqui as outras opções do menu...
-        printf("0. Sair\n");
-        printf("Escolha uma opcao: ");
-        scanf("%d", &opcao);
-
-<<<<<<< HEAD
+    int main (){
+      int opcao;
+      printf("Digite uma opção\n");
+      printf("====================\n");
+      printf("1 - Cadastrar cliente\n");
+      printf("\n");
+      printf("2 - Apagar cliente\n ");
+      printf("\n");
+      printf("3 - Listar clientes\n");
+      printf("\n");
+      printf("4 - Débito\n");
+      printf("\n");
+      printf("5 - Depósito\n");
+      printf("\n");
+      printf("6 - Extrato\n");
+      printf("\n");
+      printf("7 - Transferência entre contas");
+      printf("\n");
+      printf("8 - Sair\n");
+      printf("=====================\n");
+  }
   switch (opcao){
     case 1:
-    cadastrar_cliente();
+    printf("Cadastrar cliente");
     break;
     case 2:
     printf("Apagar cliente");
@@ -46,29 +47,8 @@ int main() {
     default:
     printf("Opção inválida");
     break;  
-    
+
   }while(opcao  != 8);
   return 0;
-=======
-        switch (opcao) {
-            case 1:
-                novoCliente(clientes, &total_clientes);
-                break;
-            case 2:
-                apagarCliente(clientes, &total_clientes);
-                break;
-            case 3:
-                listarClientes(clientes, total_clientes);
-                break;
-            // Implemente aqui os casos das outras opções do menu...
-            case 0:
-                printf("Saindo...\n");
-                break;
-            default:
-                printf("Opcao invalida!\n");
-        }
-    } while (opcao != 0);
->>>>>>> refs/remotes/origin/Maurilio
 
-    return 0;
 }
