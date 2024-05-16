9a97579e574dbb6642bf1532c9ffd6dafc992cbf
unifmausilva <161088037+unifmausilva@users.noreply.github.com>
2024-05-16 21:54:48 +0000

    funçoes 123

--- main.c
+++ main.c
@@ -1,33 +1,42 @@
-    int main (){
-      int opcao;
-      printf("Digite uma opção\n");
-      printf("====================\n");
-      printf("1 - Cadastrar cliente\n");
-      printf("\n");
-      printf("2 - Apagar cliente\n ");
-      printf("\n");
-      printf("3 - Listar clientes\n");
-      printf("\n");
-      printf("4 - Débito\n");
-      printf("\n");
-      printf("5 - Depósito\n");
-      printf("\n");
-      printf("6 - Extrato\n");
-      printf("\n");
-      printf("7 - Transferência entre contas");
-      printf("\n");
-      printf("8 - Sair\n");
-      printf("=====================\n");
-  }
-  switch (opcao){
-    case 1:
-    printf("Cadastrar cliente");
-    break;
-    case 2:
-    printf("Apagar cliente");
-    break;
-    case 3:
-    printf("Listar clientes");
+#include <stdio.h>
+#include "clientes.h"
+
+int main() {
+    Cliente clientes[MAX_CLIENTES];
+    Operacao operacoes[MAX_OPERACOES];
+    int num_clientes = 0;
+    int num_operacoes = 0;
+    int opcao;
+
+    // Carregar clientes e operações do arquivo
+    carregar_clientes(clientes, &num_clientes);
+    carregar_operacoes(operacoes, &num_operacoes);
+
+    // Loop principal do programa
+    do {
+        printf("\n===== Menu =====\n");
+        printf("1. Novo Cliente\n");
+        printf("2. Apagar Cliente\n");
+        printf("3. Listar Clientes\n");
+        printf("4. Débito\n");
+        printf("5. Depósito\n");
+        printf("6. Extrato\n");
+        printf("7. Transferência Entre Contas\n");
+        printf("0. Sair\n");
+        printf("===============\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &opcao);
+
+        switch (opcao) {
+            case 1:
+                cadastrar_cliente(clientes, &num_clientes);
+                break;
+            case 2:
+                apagar_cliente(clientes, &num_clientes);
+                break;
+            case 3:
+                listar_clientes(clientes, num_clientes);
+                break;
     break;
     case 4:
     printf("Débito");
