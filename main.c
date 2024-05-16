12a30ec47b10dc9a96c8c3d3a143c6c456c49b5c
unifmausilva <161088037+unifmausilva@users.noreply.github.com>
2024-05-16 23:13:12 +0000

    arrumando erros

--- main.c
+++ main.c
@@ -3,34 +3,20 @@
 //   gcc *.c & ./a.out
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include "banco.h"
 
-int encontrarCliente(Cliente clientes[], int total_clientes, char cpf[]) {
-    for (int i = 0; i < total_clientes; i++) {
-        if (strcmp(clientes[i].cpf, cpf) == 0) {
-            return i;
-        }
-    }
-    return -1; 
-}
+
+#include "clientes.h"
 
 int main() {
-    Cliente clientes[MAX_CLIENTES];
-    int total_clientes = carregarClientes(clientes);
-
-
     int opcao;
-    char cpf[12];
-    int indice_cliente;
-    printf("aaa%d\n",total_clientes);
+
     do {
-        printf("\nMenu de Opcoes:\n");
+        printf("\nMenu de opções:\n");
         printf("1. Novo cliente\n");
-        printf("2. Apagar cliente\n");
+        printf("2. Apaga cliente\n");
         printf("3. Listar clientes\n");
-        printf("4. Debito\n");
-        printf("5. Deposito\n");
+        printf("4. Débito\n");
+        printf("5. Depósito\n");
         printf("6. Extrato\n");
         printf("7. Transferencia Entre Contas\n");
         printf("0. Sair\n");
@@ -99,4 +85,3 @@ int main() {
 
   }while(opcao  != 8);
   return 0;
-}
