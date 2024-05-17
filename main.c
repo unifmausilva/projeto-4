b7b2496d84dc6b2cee7a55e787e97e3b86ec766d
Reinaldo2619 <161092816+Reinaldo2619@users.noreply.github.com>
2024-05-17 23:36:55 +0000

    teste

--- main.c
+++ main.c
@@ -1,21 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "banco.h"
+
+int encontrarClientePorCPF(Cliente clientes[], int total_clientes, char cpf[]) {
+    for (int i = 0; i < total_clientes; i++) {
+        if (strcmp(clientes[i].cpf, cpf) == 0) {
+            return i;
+        }
+    }
+    return -1; // Cliente não encontrado
+}
 
 int main() {
     Cliente clientes[MAX_CLIENTES];
     int total_clientes = carregarClientes(clientes);
 
     int opcao;
+    char cpf[12];
+    int indice_cliente;
+
     do {
         printf("\nMenu de Opcoes:\n");
         printf("1. Novo cliente\n");
         printf("2. Apagar cliente\n");
         printf("3. Listar clientes\n");
-        printf("4. Débito\n");
-        printf("5. Depósito\n");
+        printf("4. Debito\n");
+        printf("5. Deposito\n");
         printf("6. Extrato\n");
-        printf("7. Transferência Entre Contas\n");
+        printf("7. Transferencia Entre Contas\n");
         printf("0. Sair\n");
         printf("Escolha uma opcao: ");
         scanf("%d", &opcao);
@@ -31,22 +44,45 @@ int main() {
                 listarClientes(clientes, total_clientes);
                 break;
             case 4:
-                debito(clientes, total_clientes);
+                printf("Digite o CPF do cliente para debito: ");
+                scanf("%s", cpf);
+                indice_cliente = encontrarClientePorCPF(clientes, total_clientes, cpf);
+                if (indice_cliente != -1) {
+                    debito(&clientes[indice_cliente]);
+                } else {
+                    printf("Cliente não encontrado.\n");
+                }
                 break;
             case 5:
-                deposito(clientes, total_clientes);
+                printf("Digite o CPF do cliente para deposito: ");
+                scanf("%s", cpf);
+                indice_cliente = encontrarClientePorCPF(clientes, total_clientes, cpf);
+                if (indice_cliente != -1) {
+                    deposito(&clientes[indice_cliente]);
+                } else {
+                    printf("Cliente não encontrado.\n");
+                }
                 break;
             case 6:
-                extrato(clientes, total_clientes);
+                printf("Digite o CPF do cliente para extrato: ");
+                scanf("%s", cpf);
+                indice_cliente = encontrarClientePorCPF(clientes, total_clientes, cpf);
+                if (indice_cliente != -1) {
+                    extrato(&clientes[indice_cliente]);
+                } else {
+                    printf("Cliente não encontrado.\n");
+                }
                 break;
             case 7:
                 transferencia(clientes, total_clientes);
                 break;
             case 0:
-                printf("Saindo\n");
+                salvarClientes(clientes, total_clientes);
+                printf("Saindo...\n");
                 break;
             default:
                 printf("Opcao invalida!\n");
+                break;
         }
     } while (opcao != 0);
 
